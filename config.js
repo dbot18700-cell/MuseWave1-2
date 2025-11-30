fbba7f05184a858a97ba40ea151c0678e948e3bc
dbot18700-cell <dbot18700@gmail.com>
2025-11-30 16:11:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /**
- * Ultimate Music Bot - 
+ * MuseWave - 
  * 
  * @fileoverview 
  * @module ConfigurationManager
  * @version 1.0.0
- * @author GlaceYT
+ * @author MAYUKH ADHIKARY 
  */
 
 const EnvironmentVariableProcessor = require('process').env;
@@ -101,5 +101,6 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
